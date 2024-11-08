e4fea86def0f30e41984c4a6f360a4eb78de2d01
Julian Cuello <julianc_69@hotmail.com>
2024-11-08 11:38:29 -0300

    sanitizando juguete.controlador

--- app/controladores/juguete.api.controlador.php
+++ app/controladores/juguete.api.controlador.php
@@ -28,7 +28,45 @@ class JugueteApiControlador extends ApiControlador{
          }else{
             $this->vista->response('error not found',404);
          }
+         if(isset($_GET['sort'])&&isset($_GET['order'])){//recibir parametros
+            $sort=$_GET['sort'];//bajo a variable
+            $order=$_GET['order'];
+            switch($sort){//controlo los casos posibles
+                case 'precio':
+                    $sort='precio';
+                    break;
+                case 'idCodigoProducto':
+                    $sort='idCodigoProducto';
+                    break;
+                default:
+                    $sort='';
+                    break;
+            }
+            if(!$order=='DESC'){
+              $order='ASC';
+            }else{
+                $order='DESC';
+            }
+            $adicional='ORDER BY '.$sort.' '.$order;
+        }else{
+            $adicional='';
         }
+     }else{
+        $this->vista->response('error not found',404);
+     }
+        $lista = $this->modelo->obtenerJuguetes($adicional);
+        $this->vista->response($list, 200);
+    } else if (isset($params[':Id']) && is_numeric($params[':Id'])) {//valido parametros
+        $id = $params[':Id'];
+        $jugueteLista = $this->modelo->obtenerJuguetePorId($id);
+        if ($jugueteLista)
+            $this->vista->response($jugueteLista, 200);
+        else {
+            $this->vista->response("No existe juguete Id N°:$id", 404);
+        }
+    } else {
+        $this->vista->response('Error Not Found', 404);
+    }
 
         public function eliminarJuguete ($parametro=[]){
             if (!empty($parametro)){
@@ -38,18 +76,23 @@ class JugueteApiControlador extends ApiControlador{
                     $this->vista->response ("registro Id N°:$id eliminado.",200);
                 }
                 else {
-                    $this->vista->response ("el registro no existe", 404);
+                    $this->vista->response ("El registro Id N°:$id no existe", 404);
                 }
                 else{
-                    $this->vista->responde("error", 404);
+                    $this->vista->responde("Error not found", 404);
                 }
             }
         }
 
-       
          public function agregarJuguete(){
         
-        $juguete=$this->obtenerDatos();//desarma el json y genera un objeto
+        $juguete=$this->obtenerDatos();//obtengo el objeto
+
+        if (empty($item->idCodigoProducto) || empty($juguete->nombreProducto) || empty($juguete->precio) || empty($$juguete->material)
+            || empty($juguete->id_marca)  || empty($juguete->codigo) || empty($juguete->img) ) {//valido datos de campos recibidos
+            $this->vista->response('Faltan completar campos', 404);
+            return;
+        }
 
         $nombreProducto=$juguete->nombreProducto;
         $precio=$juguete->precio;
@@ -64,13 +107,36 @@ class JugueteApiControlador extends ApiControlador{
         }else{
             $this->view->response("el item no pudo ser ingresado",404);
         }
+        $nombreProducto=$juguete->nombreProducto;
+        $precio=$juguete->precio;
+        $material=$juguete->material;
+        $id_marca=$juguete->id_marca;
+        $codigo=$juguete->codigo;
+        $img=$juguete->img;
+        try{
+            $id = $this->modelo->insertarJuguete($nombreProducto,$precio,$material,$id_marca,$codigo,$img);
+            $nuevoJuguete=$this->model->getListById($id);
+            if ($nuevoJuguete) {
+                $this->vista->response($nuevoJuguete, 201);
+            } else {
+                $this->vista->response("El juguete no pudo ser ingresado", 404);
+            }
+        }catch(PDOException $e){//si el id de la categoria no existe o es invalido capturo error, entre otros.
+            $this->vista->response("Error al intentar ingresar el registro-$e",404);
+        }
     
     }
+
     public function modificarJuguete($parametro=[]){
         $id=$parametro[':Id'];
+        if (!empty($parametro) && is_numeric($parametro[':Id'])) {
+            $id = $parametro[':Id'];
         $jugueteId=$this->modelo->obtenerJuguetePorId($id);
         if($jugueteId){
         $juguete=$this->obtenerDatos();
+
+        $jugueteId = $this->modelo->obtenerJuguetePorId($id);
+            if ($jugueteId) {
 
         $id_juguete=$juguete->$id_juguete;
         $nombreProducto=$juguete->nombreProducto;
@@ -80,6 +146,15 @@ class JugueteApiControlador extends ApiControlador{
         $codigo=$juguete->codigo;
         $img=$juguete->img;
 
+        $juguete=$this->obtenerDatos();
+        if(
+            empty($juguete->id_juguete) || empty($juguete->nombreProducto) || empty($juguete->precio) || empty($item->material) || empty($juguete->id_marca)
+            || empty($juguete->codigo) || empty($juguete->img)
+            ) {
+                $this->vista->response('faltan completar campos', 404);
+                return;
+            }
+
         $jugueteModificado= $this->modelo->editarJuguete($id_juguete, $nombreProducto, $precio, $material, $id_marca, $codigo, $img);
         if ($jugueteModificado){
             $this->vista->response("juguete modificado con exito",200);
@@ -88,7 +163,25 @@ class JugueteApiControlador extends ApiControlador{
         }    
         }else{
             $this->vista->response("no existe juguete con Id:$id",404);
+            $id_juguete=$juguete->$id_juguete;
+            $nombreProducto=$juguete->nombreProducto;
+            $precio=$juguete->precio;
+            $material=$juguete->material;
+            $id_marca=$juguete->id_marca;
+            $codigo=$juguete->codigo;
+            $img=$juguete->img;
+
+            try {
+                $jugueteModificado= $this->modelo->editarJuguete($id_juguete, $nombreProducto, $precio, $material, $id_marca, $codigo, $img);
+                if ($jugueteModificado){
+                    $this->vista->response("juguete modificado con exito",200);
+                }else{
+                    $this->vista->response("no se pudo modificar juguete",404);
+                }    
+                }else{
+        $this->vista->response('Error Not Found', 404);
+            }
         }
     }
-}
-  
+ }
+    
--- app/controladores/marca.api.controlador.php
+++ app/controladores/marca.api.controlador.php
@@ -1,19 +1,19 @@
 <?php
 require_once './app/controladores/api.controlador.php';
 require_once './app/modelos/marca.modelo.php';
-require_once './app/modelos/marca.modelo.php';
+require_once './app/modelos/juguete.modelo.php'; 
 
 class marcaControlador extends ApiControlador{
     private $modelo;
     private $modeloJuguete;
-    public function __construct()
-    {
-        //se instancian los dos modelos para no delegar mal, y que cada modelo acceda a su tabla correspondiente.
+
+    public function __construct(){
+       
         $this->modelo = new MarcaModelo();
         $this->modeloJuguete = new JugueteModelo();
-        
     }
-    public function listaMarcas ($parametro=[]){
+
+    public function listaMarcas($parametro = []){
         if (empty($parametro)) {
             $marc = $this->modelo->obtenerMarcas();
             $this->vista->response($marc, 200);
@@ -23,50 +23,51 @@ class marcaControlador extends ApiControlador{
             if ($marca) {
                 $this->vista->response($marca, 200);
             } else {
-                $this->vista->response('no existe marca', 404);
+                $this->vista->response('No existe marca', 404);
             }
         }
     }
-    
-    public function eliminarMarca ($parametro=[]){
-        if(empty($parametro)){
-            $this->vista->response("error not found", 404);
+
+    public function eliminarMarca($parametro = []){
+        if (empty($parametro)) {
+            $this->vista->response("Error, no encontrado", 404);
             return;
         }
-        $id=$parametro['Id'];
-        try{
-            $marca=$this->modelo->borrarMarca($id);
-            if($marca){
-                $this->vista->responde('marca eliminada', 200);
-            }else{
-                $this->vista->response("error al intentar eliminar", 404);
+        $id = $parametro['Id'];
+        try {
+            $marca = $this->modelo->borrarMarca($id);
+            if ($marca) {
+                $this->vista->response('Marca eliminada', 200);
+            } else {
+                $this->vista->response("Error al intentar eliminar", 404);
             }
-        }catch (PDOException){
-            $this->vista->response("la marca que intenta eliminar, tiene asociado un conjunto de items.
-                                            Para poder eliminar correctamente,
-                                            debera eliminar los registros de los juguetes asociados/
-                                            ",404);
+        } catch (PDOException $e) {
+            $this->vista->response("La marca que intenta eliminar tiene asociado un conjunto de items. Para poder eliminar correctamente, deberá eliminar los registros de los juguetes asociados.", 404);
         }
     }
 
     public function modificarMarca(){
-        Autorizacion::verificacion();
-        try{
+        Autorizacion::verificacion(); // Verificación de autorización.
+        
+        try {
             if ($_POST && Validacion::verificacionFormulario($_POST)) {
-                $id_marca =htmlspecialchars($_POST['id_marca']);
-                $origen =htmlspecialchars($_POST['origen']);
-                $caracteristica =htmlspecialchars($_POST['caracteristica']);
-                $nombreMarca =htmlspecialchars($_POST['nombreMarca']);
-                $imgMarca =htmlspecialchars($_POST['imgMarca']);
+                $id_marca = htmlspecialchars($_POST['id_marca']);
+                $origen = htmlspecialchars($_POST['origen']);
+                $caracteristica = htmlspecialchars($_POST['caracteristica']);
+                $nombreMarca = htmlspecialchars($_POST['nombreMarca']);
+                $imgMarca = htmlspecialchars($_POST['imgMarca']);
 
-                $marcaModificada= $this->modelo->modificarJuguete($id_marca, $origen, $caracteristica, $nombreMarca, $imgMarca);
+                $marcaModificada = $this->modelo->modificarMarca($id_marca, $origen, $caracteristica, $nombreMarca, $imgMarca);
+                
                 if ($marcaModificada > 0) {
                     header('Location: ' . BASE_URL . "marca");
                 } else {
-                    $this->alertaVista->mostrarError("No se pudo actualizar marca");
+                    $this->alertaVista->mostrarError("No se pudo actualizar la marca.");
                 }
+            }
+        } catch (Exception $e) {
+            $this->vista->response("Error al modificar marca: " . $e->getMessage(), 500);
         }
     }
- 
-    
-     
+
+} 
